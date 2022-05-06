760873504015aa5237af4a8acfc8d63ef9c7ec39
pedrinhomotivado1 <104324886+pedrinhomotivado1@users.noreply.github.com>
2022-05-06 07:42:39 -0300

    Add files via upload

---
+++ jsbobar.js
@@ -0,0 +1,54 @@
+function moreinfo() {
+   if (document.querySelector('#button1').classList.contains('popup-off')) {
+      document.querySelector('#button1').classList.remove('popup-off');
+      document.querySelector('#button1').classList.add('popup-on');
+   } else {
+      document.querySelector('#button1').classList.remove('popup-on');
+      document.querySelector('#button1').classList.add('popup-off');
+   }
+}
+function moreinfo2() {
+   if (document.querySelector('#button2').classList.contains('popup-off')) {
+      document.querySelector('#button2').classList.remove('popup-off');
+      document.querySelector('#button2').classList.add('popup-on');
+   } else {
+      document.querySelector('#button2').classList.remove('popup-on');
+      document.querySelector('#button2').classList.add('popup-off');
+   }
+}
+function moreinfo3() {
+   if (document.querySelector('#button3').classList.contains('popup-off')) {
+      document.querySelector('#button3').classList.remove('popup-off');
+      document.querySelector('#button3').classList.add('popup-on');
+   } else {
+      document.querySelector('#button3').classList.remove('popup-on');
+      document.querySelector('#button3').classList.add('popup-off');
+   }
+}
+function moreinfo4() {
+   if (document.querySelector('#button4').classList.contains('popup-off')) {
+      document.querySelector('#button4').classList.remove('popup-off');
+      document.querySelector('#button4').classList.add('popup-on');
+   } else {
+      document.querySelector('#button4').classList.remove('popup-on');
+      document.querySelector('#button4').classList.add('popup-off');
+   }
+}
+function moreinfo5() {
+   if (document.querySelector('#button5').classList.contains('popup-off')) {
+      document.querySelector('#button5').classList.remove('popup-off');
+      document.querySelector('#button5').classList.add('popup-on');
+   } else {
+      document.querySelector('#button5').classList.remove('popup-on');
+      document.querySelector('#button5').classList.add('popup-off');
+   }
+}
+function moreinfo6() {
+   if (document.querySelector('#button6').classList.contains('popup-off')) {
+      document.querySelector('#button6').classList.remove('popup-off');
+      document.querySelector('#button6').classList.add('popup-on');
+   } else {
+      document.querySelector('#button6').classList.remove('popup-on');
+      document.querySelector('#button6').classList.add('popup-off');
+   }
+}
